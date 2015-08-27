d213fc4baf7dae5ea8e33faedb8ed761262eb2d6
Byunghoon Yoon <bh2yoon@gmail.com>
2015-08-26 22:47:40 -0400

    more on session; need to implement observer stuff

--- Photography/MainViewController.swift
+++ Photography/MainViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import AVFoundation
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, SessionManagerDelegate {
     private var sessionManager: SessionManager!
     
     override func viewDidLoad() {
@@ -33,10 +33,28 @@ class MainViewController: UIViewController {
             sessionManager.status = .NotAuthorized
         }
         
-        sessionManager.start()
-        
-        // enable UI
-        // ...
+        sessionManager.configure()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        sessionManager.resume()
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        sessionManager.pause()
+        super.viewWillDisappear(animated)
+    }
+    
+    
+    // MARK: session manager delegate
+    
+    private func sessionManager(sessionManager: SessionManager, shouldUpdateOrientation orientation: UIInterfaceOrientation) {
+        //
+    }
+    
+    private func sessionManager(sessionManager: SessionManager, didFailResumptionWithStatus: SessionStatus) {
+        presentViewController(UIAlertController(title: "Session resumption failed", message: nil, preferredStyle: UIAlertControllerStyle.Alert), animated: true, completion: nil)
     }
 }
 
@@ -44,31 +62,73 @@ private enum SessionStatus {
     case Success, NotAuthorized, ConfigurationFailed
 }
 
+private protocol SessionManagerDelegate {
+    func sessionManager(sessionManager: SessionManager, shouldUpdateOrientation orientation: UIInterfaceOrientation) //?
+    func sessionManager(sessionManager: SessionManager, didFailResumptionWithStatus: SessionStatus)
+}
+
 private class SessionManager {
     let session = AVCaptureSession()
     let queue = dispatch_queue_create("queue", DISPATCH_QUEUE_SERIAL)
+    
     let previewLayer: AVCaptureVideoPreviewLayer
+    var delegate: SessionManagerDelegate?
     
     var status: SessionStatus = .NotAuthorized
     private var currentInput: AVCaptureDeviceInput?
+    private var currentOutput: AVCaptureStillImageOutput?
 
     init(previewLayer layer: AVCaptureVideoPreviewLayer) {
         previewLayer = layer
         previewLayer.session = session
     }
     
-    func start() {
+    func configure() {
         dispatch_async(queue, { () -> Void in
             if self.status != .Success {
                 return
             }
             
+            // set background recording ID as UIBackgroundTaskInvalid
+            
             self.setupInput(devicePosition: .Back)
             
-            // might want to modify previewLayer.connection.videoOrientation from main queue
+            // might want to modify previewLayer.connection.videoOrientation from main queue (see line 129 in AAPLCameraViewController.m)
             
             self.setupOutput()
+            
+            self.session.commitConfiguration()
         })
+    }
+    
+    func resume() {
+        dispatch_async(queue, { () -> Void in
+            switch (self.status) {
+            case .Success:
+                self.addObservers()
+                self.session.startRunning()
+                
+            default:
+                self.delegate?.sessionManager(self, didFailResumptionWithStatus: self.status)
+            }
+        })
+    }
+    
+    func pause() {
+        dispatch_async(queue, { () -> Void in
+            if self.status == .Success {
+                self.session.stopRunning()
+                self.removeObservers()
+            }
+        })
+    }
+    
+    func addObservers() {
+        
+    }
+    
+    func removeObservers() {
+        
     }
     
     private func setupInput(devicePosition preferringDevicePosition: AVCaptureDevicePosition) {
@@ -94,12 +154,29 @@ private class SessionManager {
             
         } else {
             println("Could not add video device input to the session")
+            if let error = error {
+                println(error);
+            }
             status = .ConfigurationFailed
         }
     }
     
     private func setupOutput() {
+        if let output = currentOutput {
+            session.removeOutput(currentOutput)
+            currentOutput = nil
+        }
         
+        let stillImageOutput = AVCaptureStillImageOutput()
+        if session.canAddOutput(stillImageOutput) {
+            stillImageOutput.outputSettings = [AVVideoCodecKey: AVVideoCodecJPEG]
+            session.addOutput(stillImageOutput)
+            currentOutput = stillImageOutput
+            
+        } else {
+            println("Could not add still image output to the session")
+            status = .ConfigurationFailed
+        }
     }
 }
 
